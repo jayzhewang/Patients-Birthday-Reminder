597ab93d79fb7c389aafb6d9e57bd28d7f889214
Zhe Wang <akina.dwnhl@gmail.com>
2017-01-25 12:51:47 -0800

    added footer component

--- frontend/components/app/footer.jsx
+++ frontend/components/app/footer.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 
 const Footer = () => (
   <div className='footer'>
+    <div className='footer-logo'>
+      <img src='https://res.cloudinary.com/cloudlicious/image/upload/v1485375457/drchrono_logo_white_tan7je.png' />
+    </div>
     <div className='about'>
-
+      drreminder is a patient birthday reminder application for drchrono's providers.
+      drreminder uses Django and PostgreSQL for providing database queries and React/Redux for the views.
+      OAuth 2.0 is used to authenticate data access between the provider and the application.
     </div>
     <div className='about-links'>
-
+      Links
+      <ul id='about-links'>
+        <li><a href='https://github.com/jz-wang'>Github</a></li>
+        <li><a href='https://www.linkedin.com/in/zhe-wang-b282b4127/'>LinkedIn</a></li>
+        <li><a href='http://www.jayzhewang.com/'>Portfolio</a></li>
+      </ul>
     </div>
   </div>
 );

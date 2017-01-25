d8f689a9e4699b604d771de4bb39c9de6eea53ff
Zhe Wang <akina.dwnhl@gmail.com>
2017-01-25 13:34:34 -0800

    added welcome img

--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import dc from '../../../env';
 
 const Welcome = () => (
-  <div>
-    Welcome to drchrono's app!!!!
-    <a href={`https://drchrono.com/o/authorize/?redirect_uri=${encodeURIComponent(dc.REDIRECT_URI)}&response_type=code&client_id=${dc.CLIENT_ID}&scope=${encodeURIComponent(dc.SCOPES)}`}>drchrono oauth</a>
+  <div className='welcome'>
+    <div className='login-in'>
+      Welcome to drchrono's app!!!!
+      <a href={`https://drchrono.com/o/authorize/?redirect_uri=${encodeURIComponent(dc.REDIRECT_URI)}&response_type=code&client_id=${dc.CLIENT_ID}&scope=${encodeURIComponent(dc.SCOPES)}`}>drchrono oauth</a>
+    </div>
+    <div className='artwork'>
+      <img src='https://res.cloudinary.com/cloudlicious/image/upload/v1485379033/san_francisco_mi5gdq.png' />
+    </div>
   </div>
 );
 

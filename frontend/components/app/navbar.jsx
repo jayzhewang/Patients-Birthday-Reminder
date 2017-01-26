b3968f85748d1fd5f1376e9025c92ed4efe62bfa
Zhe Wang <akina.dwnhl@gmail.com>
2017-01-25 21:42:13 -0800

    changed navbar to fixed

--- frontend/components/app/navbar.jsx
+++ frontend/components/app/navbar.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Navbar = () => (
   <div className='nav-bar'>
-    <div className='logo'>
-      <img src='https://res.cloudinary.com/cloudlicious/image/upload/v1485322744/dr_reminder_bq5pkw.png' />
-    </div>
-    <div className='links'>
-      <ul id='links'>
-        <li>About</li>
-        <li><a id='a' href='https://www.drchrono.com/'>drchrono</a></li>
-        <div className='signup-button'>
-          <div><a id='a' href='https://www.drchrono.com/sign-up/'>Sign Up for Free</a></div>
-        </div>
-      </ul>
+    <div className='nav-container'>
+      <div className='logo'>
+        <img src='https://res.cloudinary.com/cloudlicious/image/upload/v1485322744/dr_reminder_bq5pkw.png' />
+      </div>
+      <div className='links'>
+        <ul id='links'>
+          <li>About</li>
+          <li><a id='a' href='https://www.drchrono.com/'>drchrono</a></li>
+          <div className='signup-button'>
+            <div><a id='a' href='https://www.drchrono.com/sign-up/'>Sign Up for Free</a></div>
+          </div>
+        </ul>
+      </div>
     </div>
   </div>
 );
